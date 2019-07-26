9f21131b5558207674efe82140c9c02135e7a694
Ghosh <764976@cognizant.com>
2019-07-26 16:13:29 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,11 +23,12 @@ import { CmspageModule } from './cmspage/cmspage.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BlogpostModule,
     CmspageModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/blogpost/blogpost.service.ts
+++ src/app/blogpost/blogpost.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Blogpost } from './blogpost';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogpostService {
 
-  constructor() { }
+  Serverurl = 'http://localhost/dev/blogger/';
+  errordata: {};
+
+  constructor(private http:HttpClient) { }
+
+  getBlogs() {
+    return this.http.get<Blogpost>(this.Serverurl+'api/blogs').pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  
+  getFeaturedBlogs() {
+    return this.http.get<Blogpost>(this.Serverurl+'api/featured_blogs').pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  private handleError(error: HttpErrorResponse){
+      if (error.error instanceof ErrorEvent){
+        console.error('An Error occupied',error.error.message);
+      }
+      else {
+        console.error(`Backend returned code ${error.status},`+`body was: ${error.error}`);
+      }
+
+      this.errordata = {
+        errorTitle: 'Oops: Request for Document failed',
+        errorDesc: 'Something bad happended,Please try again later'
+      };
+
+      return throwError(this.errordata);
+  }
 }
